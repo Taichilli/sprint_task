35110ef1b32cc9d759598ee66716f929eb3f5b80
Taichilli <aleksalex190@gmail.com>
2025-07-01 22:23:56 +0500

    дописал пропущенный timestamp

--- decor_task3.py
+++ decor_task3.py
@@ -1,17 +1,20 @@
+from datetime import datetime, timezone
+
 one = float(input('введите число'))
 two = float(input('введите число'))
 
 def log_function(file):
     def actual(func):
         def wrapper(*args, **kwargs):
-            with open(file, 'a+') as f:
-                f.write(f'\nTrace: called {func.__name__}()'
-                  f' with args: {args}, kwargs: {kwargs}\n')
+            with open(file, 'a+',encoding='utf-8') as f:
+
+                f.write(f'\nИмя функции: {func.__name__}()'
+                f' Входные данные args: {args}, kwargs: {kwargs}| время запуска: {datetime.now(timezone.utc)}--> UTC\n')
 
                 output = func(*args, **kwargs)
 
-                f.write(f'Trace: called {func.__name__}()'
-                  f' with output: {output}\n')
+                f.write(f'Имя функции: {func.__name__}()'
+                f' Выходные данные: {output}| Локальное время: {datetime.now(timezone.utc).astimezone()}\n')
 
             return output
         return wrapper
